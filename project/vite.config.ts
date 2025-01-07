a01c4f6c1ad87d023dc7f23653016088e535fb36
charhateom <omcharhate45@gmail.com>
2025-01-07 22:56:20 +0530

    host

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    target: 'esnext', // Ensure modern JS output
+    rollupOptions: {
+      output: {
+        format: 'es', // Ensure the output is in the ES module format
+      },
+    },
   },
 });
